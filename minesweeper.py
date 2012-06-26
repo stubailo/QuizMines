794525182ba7f2d10c776dc9c45c026d5782b2e9
Jason Zhao <jasonz@palantir.com>
2012-06-25 20:44:28 -0700

    fixed expansion

--- minesweeper.py
+++ minesweeper.py
@@ -30,51 +30,57 @@ def inBound(x, y):
 	return (x >= 0 and x < WIDTH and y>= 0 and y < HEIGHT)
 
 def compute_mine(x, y):
+	if gameMap[x][y] == -1 :
+		return -1
 	mines = 0
 	for i in range(x-1,x+2):
 		for j in range (y-1, y+2):
 			if inBound(i,j) and gameMap[i][j] == -1:
 				mines = mines + 1
 	return mines
-				
-def compute_state(x,y,recurse):
+
+def compute_state(x,y):
 	if inBound(x,y):
 		if gameMap[x][y] != None :
 			return -2
 		mines = compute_mine(x,y)
 		if mines == -1 :
 			return -1
-		elif mines == 0 or recurse:
+		else:
 			gameMap[x][y] = mines
-			compute_state(x-1, y, False)
-			compute_state(x, y-1, False)
-			compute_state(x-1, y-1, False)
-			compute_state(x+1, y, False)
-			compute_state(x, y+1, False)
-			compute_state(x+1, y+1, False)
-			compute_state(x-1, y+1, False)
-			compute_state(x+1, y-1, False)
+			if mines == 0 :
+				compute_state(x-1, y)
+				compute_state(x, y-1)
+				compute_state(x-1, y-1)
+				compute_state(x+1, y)
+				compute_state(x, y+1)
+				compute_state(x+1, y+1)
+				compute_state(x-1, y+1)
+				compute_state(x+1, y-1)
+			return mines
+def expand(x,y):
+	mines = compute_mine(x,y)
+	if mines != 0 :
+		gameMap[x][y] = mines
+	for i in range(x-1,x+2):
+		for j in range (y-1, y+2):
+			if inBound(i,j) and compute_mine(i,j) == 0 :
+				compute_state(i,j)		
 	
 @app.route('/', methods=['POST', 'GET'])
 def mine_server():
-	resp = 0
 	new_ip = request.access_route[0]
 	if new_ip in ips :
 		print "existing ip"
 		# need logic
 	else:
 		ips.add(new_ip)
-	x = int(request.args.get('x'))
-	y = int(request.args.get('y'))
-	if inBound(x, y):
-		resp = compute_state(x,y, True)
-	response = ""
-	for ip in request.access_route :
-		response = response + ip + "</br>"
-	for i in range(0, WIDTH):
-		response = response + str(gameMap[i]) + "</br>"
-	print response
-	rep = make_response(json.dumps({"state" : gameMap, "response" : resp}))
+	if request.args.has_key('x'):
+		x = int(request.args.get('x'))
+		y = int(request.args.get('y'))
+		if inBound(x, y):
+			expand(x,y)
+	rep = make_response(json.dumps({"state" : gameMap}))
 	rep.headers['Access-Control-Allow-Origin'] = "*"
 	return rep
 
